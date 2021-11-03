aca1d4f63745bc0addb8106349ee2588aa920b01
geoffreyweal <geoffrey.weal@gmail.com>
2021-11-03 01:54:13 -0700

    update version of Adsorber v1.10

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(name='Adsorber',
       license='GNU AFFERO GENERAL PUBLIC LICENSE',
       zip_safe=False,
       keywords = ['Adsorber', 'ase', 'university-of-otago', 'atomic-simulation-environment', 'otago-university'],
-      install_requires=['numpy','scipy','matplotlib','ase>=3.19.0','openpyxl','packaging', 'networkx'],
+      install_requires=['numpy','scipy','matplotlib','ase>=3.19.0','openpyxl','packaging', 'networkx, tqdm'],
       classifiers=[
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Science/Research',      # Define that your audience are developers
